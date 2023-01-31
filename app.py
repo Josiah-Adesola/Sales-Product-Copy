09c9013393d527cd18fc9c15b1b935146de2d55a
Josiah Adesola <59745353+Josiah-Adesola@users.noreply.github.com>
2023-01-31 15:16:00 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+import openai
+import streamlit as st
+
+openai.api_key = "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"
+
+def main():
+    st.title("Product Description Generator")
+    st.image("product.jpg")
+    notes = st.text_area("Enter a product information:")
+    if st.button("Generate Description"):
+        with st.spinner("Generating description..."):
+            response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"Write a production description based on the below information:\n\nProduct: {notes}",
+            temperature=0.8,
+            max_tokens=503,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+            )
+            
+            description = response['choices'][0]['text']
+            st.subheader("Generated description: ")
+            st.success(description)
+            
+        
+if __name__ == "__main__":
+    main()
